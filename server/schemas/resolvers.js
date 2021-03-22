9c99606a97be0d7dd048b27902d2ce8a760c92ab
jbeedle19 <josh.beedle@gmail.com>
2021-03-22 12:05:19 -0400

    finished User and Thought queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,9 +2,28 @@ const { User, Thought } = require('../models');
 
 const resolvers = {
     Query: {
+        // Get all Users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // Get a single User by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // Get all thoughts / a user's thoughts
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
+        },
+        // Get single thought by ID
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
         }
     }
 };
