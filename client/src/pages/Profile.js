13583e0956f6ea8cd3d8dd8d4c2646406579cd13
jbeedle19 <josh.beedle@gmail.com>
2021-03-25 11:19:48 -0400

    outlined ThoughtForm component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,6 +6,7 @@ import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import { ADD_FRIEND } from '../utils/mutations';
 import FriendList from '../components/FriendList';
 import Auth from '../utils/auth';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -71,6 +72,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
